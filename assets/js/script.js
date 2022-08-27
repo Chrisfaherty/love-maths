ece1690df3064d6764bf6b6f5f509de9c7cf4e7b
Chrisfaherty <chrismactom@gmail.com>
2022-08-27 07:47:15 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame(){
+
+}
+
+function checkAnswers(){
+
+}
+
+function calculateCorrectAnswers(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
